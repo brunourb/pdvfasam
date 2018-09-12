234a7bafde32a2e09c1a7e6211628c41de41bb1d
Bruno Rodrigues <bruno@urbano.eti.br>
2018-09-11 22:04:06 -0300

    * Another example about adapters. * Example to load image from image resource in the project.

--- app/src/main/java/br/edu/fasam/mobile/pdvfasam/activities/ProdutoActivity.java
+++ app/src/main/java/br/edu/fasam/mobile/pdvfasam/activities/ProdutoActivity.java
@@ -6,6 +6,12 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 import br.edu.fasam.mobile.pdvfasam.R;
 import br.edu.fasam.mobile.pdvfasam.config.DebugActivity;
@@ -20,6 +26,7 @@ public class ProdutoActivity extends DebugActivity {
     protected EditText txtQtd;
     protected EditText txtValorUnitario;
     protected ListView listView;
+    protected List<HashMap<String,String>> listaHasMap = new ArrayList<HashMap<String,String>>();
 
 
     @Override
@@ -31,9 +38,49 @@ public class ProdutoActivity extends DebugActivity {
     public void addProduct(View view) {
 
         //CAPTURAR DADOS DA TELA
+        txtNome = findViewById(R.id.txtNome);
+        txtDescricao = findViewById(R.id.txtDescricao);
+        txtNcm = findViewById(R.id.txtNcm);
+        txtCodBarra = findViewById(R.id.txtCodBarra);
+        imgViewFoto = findViewById(R.id.imgViewFoto);
+        txtQtd = findViewById(R.id.txtQtd);
+        txtValorUnitario = findViewById(R.id.txtValorUnitario);
+        listView = findViewById(R.id.listView);
 
+        String nome, descricao, ncm, codBarra;
+        BigDecimal qtd = BigDecimal.ZERO;
+        BigDecimal valorUnitario = BigDecimal.ZERO;
         //Processar os dados
+        nome = txtNome.getText().toString();
+        descricao = txtDescricao.getText().toString();
+        ncm = txtNcm.getText().toString();
+        codBarra = txtCodBarra.getText().toString();
 
+        qtd = new BigDecimal(txtQtd.getText().toString());
+        valorUnitario = new BigDecimal(txtValorUnitario.getText().toString());
+
+        BigDecimal valorTotal = qtd.multiply(valorUnitario);
+
+        imgViewFoto.setImageResource(R.drawable.meme_obama);
+
+        HashMap<String,String> map = new HashMap<String,String>();
+        map.put("nome",nome);
+        map.put("descricao",descricao);
+        map.put("ncm",ncm);
+        map.put("codBarra",codBarra);
+        map.put("qtd",qtd.toPlainString());
+        map.put("valorUnitario",valorUnitario.toPlainString());
+
+        listaHasMap.add(map);
+
+
+        String[] from = {"nome","descricao"};
+        int[] to = {R.id.txtViewNome,R.id.txtViewDescricao};
+
+        SimpleAdapter simpleAdapter = new SimpleAdapter(this,listaHasMap,
+                R.layout.item_produto,from,to);
+
+        listView.setAdapter(simpleAdapter);
 
         //Entregar os dados para o usuario
     }
