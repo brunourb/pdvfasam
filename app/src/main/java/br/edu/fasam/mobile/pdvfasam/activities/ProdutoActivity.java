7badddb400638e045891071d30569430e09198e9
Bruno Rodrigues <bruno@urbano.eti.br>
2018-09-11 21:29:01 -0300

    * Create attributes to ProductActivity receive data.

--- app/src/main/java/br/edu/fasam/mobile/pdvfasam/activities/ProdutoActivity.java
+++ app/src/main/java/br/edu/fasam/mobile/pdvfasam/activities/ProdutoActivity.java
@@ -3,11 +3,24 @@ package br.edu.fasam.mobile.pdvfasam.activities;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
 
 import br.edu.fasam.mobile.pdvfasam.R;
 import br.edu.fasam.mobile.pdvfasam.config.DebugActivity;
 
 public class ProdutoActivity extends DebugActivity {
+
+    protected EditText txtNome;
+    protected EditText txtDescricao;
+    protected EditText txtNcm;
+    protected EditText txtCodBarra;
+    protected ImageView imgViewFoto;
+    protected EditText txtQtd;
+    protected EditText txtValorUnitario;
+    protected ListView listView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +29,12 @@ public class ProdutoActivity extends DebugActivity {
     }
 
     public void addProduct(View view) {
-        //
+
+        //CAPTURAR DADOS DA TELA
+
+        //Processar os dados
+
+
+        //Entregar os dados para o usuario
     }
 }
